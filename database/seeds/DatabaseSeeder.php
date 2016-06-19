d52b982f1e46ef46a594749749145d56b56dcb39
Eleclare <kerokaznu2@gmail.com>
2016-06-19 17:10:34 +0200

    complete

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+require("UserTableSeeder.php");
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(UserTableSeeder::class);
+    }
+}
